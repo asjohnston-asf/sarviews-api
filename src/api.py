878dae0691b0cbf8e6b5af5d6c91d2a9fd452b04
Andrew Johnston <asjohnston@alaska.edu>
2021-01-26 14:14:40 -0900

    update /recent_products to return all products processed in the last 7 days

--- src/api.py
+++ src/api.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta, timezone
 from decimal import Decimal
 from os import environ
 
@@ -60,12 +61,11 @@ def get_event_by_id(event_id):
 @app.route('/recent_products')
 def get_recent_products():
     table = dynamodb.Table(environ['PRODUCTS_TABLE_NAME'])
-    key_expression = Key('status_code').eq('SUCCESSFUL')
+    one_week_ago = (datetime.now(timezone.utc) - timedelta(days=7)).strftime('%Y-%m-%dT%H:%M:%SZ')
+    key_expression = Key('status_code').eq('SUCCEEDED') & Key('processing_date').gte(one_week_ago)
     response = table.query(
         IndexName='processing_date',
         KeyConditionExpression=key_expression,
-        Limit=100,
-        ScanIndexForward=False,
     )
     return jsonify(response['Items'])
 
